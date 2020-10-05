eeb6bf481eceb0256d0e5a45089cb4b48ff5371e
Prashant Singh <prashant.101386@gmail.com>
2020-10-06 01:51:11 +0530

    Updated code

--- Learning/DIH_ADF/variables.tf
+++ Learning/DIH_ADF/variables.tf
@@ -2,42 +2,6 @@
 variable "resource_group_name" {
   description = "Resource group for shared ADF"
 }
-
-variable "bes_resource_group_name" {
-
-}
-
-variable "bes_account_name" {
-
-}
-
-variable "contributor_access_objid" {}
-
-variable "access_key" {
-
-}
-
-variable "integration_runtime_name" {}
-
-variable "adls_secret_name" {
-
-}
-variable "diagnostic_setting_name" {}
-
-variable "data_lake_base_url" {}
-
-variable "keyvault_linked_service_name" {}
-          
-variable "data_lake_linked_service_name" {}
-
-variable "keyvault_container_name" {
-
-}
-
-variable "adls_container_name" {
-
-}
-
 variable "data_factory_name" {}
 
 variable "subscription_id" {
@@ -58,12 +22,4 @@ variable "tenant_id" {
 
 variable "tags" {
   
-}
-
-variable "backend_state" {
-  
-}
-
-variable "log_analytics_workspace_id" {
-  description = "(optional) describe your variable"
 }
