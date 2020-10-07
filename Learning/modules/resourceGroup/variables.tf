6946e407c6c8c889468786637293fab77c0e85aa
Prashant Singh <prashant.101386@gmail.com>
2020-10-07 11:57:13 +0530

    Updated code

--- Learning/modules/resourceGroup/variables.tf
+++ Learning/modules/resourceGroup/variables.tf
@@ -6,7 +6,3 @@ variable "resource_group_location" {
   description = "The location of the resource group"
   default     = ["West US 2"]  
 }
-
-variable "tags" {
-  description = "Resource tag key pair values from terraform.tfvars file"
-}
