d2330186c330f22ff6417be788b1a8da2911d1eb
Prashant Singh <prashant.101386@gmail.com>
2020-10-07 12:46:54 +0530

    Update

--- Learning/modules/logicApp/main.tf
+++ Learning/modules/logicApp/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_logic_app_workflow" "logic_app" {
   name                    = var.logic_app_name
-  location                = "westus2"
+  location                = "westindia"
   resource_group_name     = var.resource_group_name
 }
