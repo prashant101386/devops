341db80f6a699641766272af193ddc76f0e55b99
Prashant Singh <prashant.101386@gmail.com>
2020-10-07 12:20:58 +0530

    Update

--- Learning/modules/logicApp/main.tf
+++ Learning/modules/logicApp/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_logic_app_workflow" "logic_app" {
   name                    = var.logic_app_name
   resource_group_location = "westus2"
-  resource_group_name     = shared_adf_resource_group.resource_group_name
+  resource_group_name     = var.resource_group_name
 }
