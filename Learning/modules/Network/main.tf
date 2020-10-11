a56c9eca5ec38459e98c36d8a9fbf985388a5b2f
Prashant Singh <prashant.101386@gmail.com>
2020-10-11 15:31:56 +0530

    update

--- Learning/modules/Network/main.tf
+++ Learning/modules/Network/main.tf
@@ -1,7 +1,8 @@
 resource "azurerm_virtual_network" "vn" {
   name          = var.virtual_network_name
+  resource_group_name = var.resource_group_name
   address_space = ["10.1.2.0/24"]
-  location      = var.virtual_network_location
+  location      = var.resource_group_location
 
   subnet {
     name    = var.subnet_name
