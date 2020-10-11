2ff850ed87946cc6bb81e84e5319beb230574f3e
Prashant Singh <prashant.101386@gmail.com>
2020-10-11 15:20:31 +0530

    Update

--- Learning/modules/Network/outputs.tf
+++ Learning/modules/Network/outputs.tf
@@ -1,11 +1,11 @@
 output "virtual_network_name" {
-  value = azure_virtual_network.vn.name
+  value = azurerm_virtual_network.vn.name
 }
 
 output "virtual_network_location" {
-  value = azure_virtual_network.vn.name
+  value = azurerm_virtual_network.vn.name
 }
 
 output "subnet_name" {
-  value = azure_virtual_network.vn.subnet_name
+  value = azurerm_virtual_network.vn.subnet_name
 }
