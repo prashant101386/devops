4665a4dd43b1b7fdc49e0aa501e2f9b03e7b0877
Prashant Singh <prashant.101386@gmail.com>
2020-10-11 13:05:58 +0530

    Update

--- Learning/modules/webApp/outputs.tf
+++ Learning/modules/webApp/outputs.tf
@@ -1,5 +1,9 @@
-output "logic_app_name" {
-  value = azurerm_logic_app_workflow.logic_app.name
+output "app_service_plan" {
+  value = azurerm_app_service_plan.asp.name
+}
+
+output "app_service_name" {
+  value = azurerm_app_service.asp.name
 }
 
 output "resource_group_name" {
