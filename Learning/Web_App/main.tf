5e7bdc4d04bcca025aa5ba120fe4537564d8bbad
Prashant Singh <prashant.101386@gmail.com>
2020-10-11 16:38:58 +0530

    Update

--- Learning/Web_App/main.tf
+++ Learning/Web_App/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
   features{}
 }
-
+/*
 ####################################################################Resource Group
   module "resource_group" {
   source = "../modules/resourceGroup"
@@ -14,14 +14,15 @@ provider "azurerm" {
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
   }
+  */
 ####################################################################App Service
   module "app_service" {
   source = "../modules/webApp"
 
   app_service_plan        = var.app_service_plan
   app_service_name        = var.app_service_name
-  resource_group_name     = module.resource_group.resource_group_name
-  resource_group_location = module.resource_group.resource_group_location
+  resource_group_name     = var.resource_group_name
+  resource_group_location = var.resource_group_location
 }
 
 ################################################################################Key Vault Access
